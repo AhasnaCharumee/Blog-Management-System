4fc55f70ace8302a8f316b5ba60b05f7c44fa97e
AhasnaCharumee <charumeeahasna@gmail.com>
2025-08-16 10:57:01 +0530

    updated pages look

--- src/main/java/lk/ijse/gdse72/blog_management/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse72/blog_management/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/index.html")
                 );
         return http.build();
     }
